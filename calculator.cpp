f7e23351b1ab054542de1bc4cd8a3e80268300d8
binhvu3 <binhhtvu@gmail.com>
2023-12-07 16:23:24 -0800

    final project edit

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
 int main(){
+  double num1, num2;
+  char operation;
+
+  std::cout << "Enter first num: ";
+  std::cin >> num1;
+
+  std::cout << "Enter second num: ";
+  std::cin >> num2;
+
+  std::cout << "Enter the operation (+, -, *, /): ";
+  std::cin >> operation;
+
+  switch (operation) {
+    case '+':
+      std::cout << "Result: " << num1+num2; break;
+
+    case '-':
+      std::cout << "Result: " << num1-num2; break;
+
+    case '*':
+      std::cout << "Result: " << num1*num2; break;
+
+    case '/':
+      if (num2 != 0){
+        std::cout << "Result: " << num1/num2; break;
+      }
+      else{
+        std::cout << "Error: Divide by zero!!!"; break;
+      }
+    default:
+      std::cout << "Invalid Input!" << std::endl;
+  }
   return 0;
 }
