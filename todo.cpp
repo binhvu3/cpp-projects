0de5f93492b73f2299adaa1b49641506cd52cc48
binhvu3 <binhhtvu@gmail.com>
2023-12-08 16:26:27 -0800

    save progress, load saved tasks from file

---
+++ todo.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void showTasks(const std::vector<std::string> &tasks){
+  std::cout << "To-Do List: " << std::endl;
+  for {
+    (int i=0; i + 1 << ". " << tasks[i] << std::endl);
+  }
+}
+
+int main(){
+  std::vector<std::string> tasks;
+  std::string task;
+  char choice;
+
+  // Load existing tasks from file
+  std::ifstream inputFile("tasks.txt")
+
+  return 0;
+}
