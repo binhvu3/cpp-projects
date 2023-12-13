0187380f825b9fb35922c3a8cc03dbcb1996e343
binhvu3 <binhhtvu@gmail.com>
2023-12-13 15:41:34 -0800

    playable version

--- 6_tic_tac_toes/tic_tac_toes.cpp
+++ 6_tic_tac_toes/tic_tac_toes.cpp
@@ -1,3 +1,9 @@
+/*
+ * TODO: 
+ * 1. Invalid user input check
+ * 2. Recuring game after win:w
+ *
+*/
 #include <iostream>
 #include <vector>
 
@@ -22,25 +28,28 @@ class TicTac{
   bool checkWinner(const std::vector<std::vector<char>> &board, char player){
     for (int i = 0; i<ROWS; i++){
       if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
-          (board[0][i] == player && board[1][i] == player && board[2][i] == player){
+          (board[0][i] == player && board[1][i] == player && board[2][i] == player)){
         return true;
       }
     }
 
     if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
-        (board[0][2] == player && board[1][1] == player && board[2][0] == player){
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)){
       return true;
     }
 
     return false;
   }
 
-  void markBoard(const std::vector<std::vector<char>> &board, char player){
-
-  }
-
-  void resetBoard(const std::vector<std::vector<char>> &board){
-
+  char checkDraw(const std::vector<std::vector<char>> &board){
+    for (const auto &row : board){
+      for (char cell : row){
+        if (cell == ' '){
+          return false;
+        }
+      }
+    }
+    return true;
   }
 };
 
@@ -48,9 +57,37 @@ class TicTac{
 int main(){
   TicTac game;
   std::vector<std::vector<char>> board(3, std::vector<char>(3, ' '));
-  board[0][0] = 'x';
-  board[1][1] = 'y';
-  game.printBoard(board);
+  char currPlayer = 'X';
+  int row, col;
+  bool gameOver = false;
+
+  while (!gameOver){
+    game.printBoard(board);
+    std::cout << "Player " << currPlayer << ", enter row (0-2): ";
+    std::cin >> row;
+
+    std::cout << "Player " << currPlayer << ", enter col (0-2): ";
+    std::cin >> col;
+
+    if (board[row][col] == ' '){
+      board[row][col] = currPlayer;
+      gameOver = game.checkWinner(board, currPlayer);
+
+    if (gameOver){
+      game.printBoard(board);
+      std::cout << "Player " << currPlayer << " wins!" << std::endl;
+    }
+    else if (game.checkDraw(board)){
+      std::cout << "It's a draw!" << std::endl;
+      gameOver = true;
+    }
+    currPlayer = (currPlayer == 'X') ? 'O' : 'X';
+    }
+    else{
+      std::cout << "That spot is already taken. Try again." << std::endl;
+    }
+  }
+
 
   return 0;
 }
